02a9d53772faa8ce3c0f5faedd466aff83b902bd
JiaXun <jiaxun_jx@126.com>
2012-09-04 14:22:23 +0800

    fix some empty folder

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+    res.render('index', { title: 'Express' });
 };
+
+exports.saveEmperor = function(req, res) {
+    res.send('save empire');
+    res.end();
+};
+
+exports.loadEmperor = function(req, res) {
+    var list = [
+        {},
+        {}
+    ];
+    res.send('');
+}
